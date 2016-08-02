3f0a50aec6d1951e840f606f280d04db2a31d098
Tony <tbenny001@gmail.com>
2016-08-02 00:36:04 -0400

    Adding insert function to linked list class

--- LinkedList.js
+++ LinkedList.js
@@ -5,16 +5,30 @@ function LinkedList() {
     this.length = 0;
 }
 
-LinkedList.prototype.addNode = function(newNode) {
+LinkedList.prototype.push = function(newNode) {
     newNode.next = this.head;
     this.head = newNode;
+    this.length++;
+}
+
+LinkedList.prototype.insert = function(index, newNode) {
+    var currentNode = this.head;
+    for(var i = 1; i < index; i++) {
+        currentNode = currentNode.next;
+    }
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
     this.length++;
 }
 
 var list1 = new LinkedList();
 
 for(var i = 0; i < 10; i++) {
-    list1.addNode(new Node(i));
+    list1.push(new Node(i));
 }
 
+list1.insert(1, new Node(13));
+
 console.log(list1.head);
+console.log(list1.length);
+console.log(list1);
