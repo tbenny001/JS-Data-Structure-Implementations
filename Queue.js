d39ab75ed8bd652cfb12ea9003370b6a45a41e9b
Tony <tbenny001@gmail.com>
2016-08-13 23:02:00 -0400

    Creating stack and queue classes

---
+++ Queue.js
@@ -0,0 +1,3 @@
+var should = require('should');
+var Node = require('./../ListNode');
+var LinkedList = require('./../LinkedList');
---
+++ Stack.js
@@ -0,0 +1,55 @@
+var should = require('should');
+var Node = require('./../ListNode');
+var LinkedList = require('./../LinkedList');
+
+describe('Given a new Stack', function() {
+    describe('When using the top function', function() {
+        it('Then it should return null', function() {
+
+        });
+    });
+    describe('When using the push function', function() {
+        it('Then it should add the new item to the stack', function() {
+
+        });
+        it('Then the top variable should be set to the new item', function() {
+
+        });
+    });
+    describe('When using the pop function', function() {
+        it('Then it should return null', function() {
+
+        });
+    });
+    describe('When using the isEmpty function', function() {
+        it('Then it should return true', function() {
+
+        });
+    });
+});
+
+describe('Given a non-empty Stack', function(){
+    describe('When using the top function', function() {
+        it('Then it should return the item that the top variable points to', function() {
+
+        });
+    });
+    describe('When using the push function', function() {
+        it('Then it should add the new item to the stack', function() {
+
+        });
+        it('Then the top variable should be set to the new item', function() {
+
+        });
+    });
+    describe('When using the pop function', function() {
+        it('Then it should return the item that the top variable points to', function() {
+
+        });
+    });
+    describe('When using the isEmpty function', function() {
+        it('Then it should return false', function() {
+
+        });
+    });
+});
