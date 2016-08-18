35597ef225969543e0380b5bc56f577509b0556f
Tony <tbenny001@gmail.com>
2016-08-18 09:46:05 -0400

    Finishing empty stack unit tests

--- Stack.js
+++ Stack.js
@@ -6,12 +6,13 @@ function Stack() {
 
 Stack.prototype = new LinkedList();
 
-Stack.prototype.push = function(data) {
-
+Stack.prototype.push = function(newNode) {
+    this.add(newNode);
+    this.top = this.head;
 }
 
 Stack.prototype.pop = function() {
-
+    return null;
 }
 
 Stack.prototype.isEmpty = function() {
--- test/Stack.spec.js
+++ test/Stack.spec.js
@@ -4,32 +4,36 @@ var LinkedList = require('./../LinkedList');
 var Stack = require('./../Stack');
 
 describe('Given a new Stack', function() {
+    var stack1;
+    var newNode;
     beforeEach(function() {
-        var stack1 = new Stack();
-        
-        stack1.push(1);
+        stack1 = new Stack();
     });
     describe('When using the top function', function() {
         it('Then it should return null', function() {
-            stack1.top.should.be.equal(null);
-        });
-    });
-    describe('When using the push function', function() {
-        it('Then it should add the new item to the stack', function() {
-            stack1.length.should.be(1);
-        });
-        it('Then the top variable should be set to the new item', function() {
-            stack1.top.should.equal();
-        });
-    });
-    describe('When using the pop function', function() {
-        it('Then it should return null', function() {
-
+            should.not.exist(stack1.top);
         });
     });
     describe('When using the isEmpty function', function() {
         it('Then it should return true', function() {
             stack1.isEmpty().should.be.true();
+        });
+    });
+    describe('When using the pop function', function() {
+        it('Then it should return null', function() {
+            should.not.exist(stack1.pop());
+        });
+    });
+    describe('When using the push function', function() {
+        beforeEach(function() {
+            newNode = new Node(1);
+            stack1.push(newNode);
+        });
+        it('Then it should add the new item to the stack', function() {
+            stack1.length.should.be.equal(1);
+        });
+        it('Then the top variable should be set to the new item', function() {
+            stack1.top.should.equal(newNode);
         });
     });
 });
