9f32bb8d1019bb035e904dc2407517404c4dff80
Tony <tbenny001@gmail.com>
2016-08-21 16:05:08 -0400

    Writing test for find on BinaryTree

--- BinaryTree.js
+++ BinaryTree.js
@@ -4,4 +4,13 @@ function BinaryTree(root) {
     this.root = root;
 }
 
+BinaryTree.prototype.find = function() {
+    if(this.root === null) {
+        var error = new Error('Unable to search an empty tree');
+        throw error;
+    } else {
+        
+    }
+}
+
 module.exports = BinaryTree;
--- test/BinaryTree.spec.js
+++ test/BinaryTree.spec.js
@@ -1,3 +1,20 @@
 var should = require('should');
 var TreeNode = require('./../TreeNode');
 var BinaryTree = require('./../BinaryTree');
+
+describe('Given an empty Binary Tree', function() {
+    var newTree;
+    beforeEach(function() {
+        newTree = new BinaryTree();
+    });
+    describe('When using the getRoot method', function() {
+        it('Then it should retrun null', function() {
+            should.not.exist(newTree.root);
+        });
+    });
+    describe('When using the find method', function() {
+        it('Then it should throw an error', function() {
+            should.throws(newTree.find(2), Error, 'Unable to search an empty tree');
+        });
+    });
+});
